919de5b6461037400e3843b5a189f169a6312af7
aroua <aroua.ferchichi@esprit.tn>
2024-11-07 17:41:27 +0100

    fix1

--- src/main/java/tn/esprit/tpfoyer/service/FoyerServiceImpl.java
+++ src/main/java/tn/esprit/tpfoyer/service/FoyerServiceImpl.java
@@ -1,8 +1,10 @@
 package tn.esprit.tpfoyer.service;
 
 
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import tn.esprit.tpfoyer.entity.Foyer;
 import tn.esprit.tpfoyer.repository.FoyerRepository;
@@ -11,6 +13,8 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Transactional
+@Slf4j
 public class FoyerServiceImpl implements IFoyerService {
 
     FoyerRepository foyerRepository;
