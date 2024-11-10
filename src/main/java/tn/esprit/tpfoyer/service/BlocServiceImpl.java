c9b79a1aef3aa371d842dd09475d4bb1b4fe65f3
ameni <ameni.aloui2@esprit.tn>
2024-11-10 18:10:12 +0100

    modifcode3

--- src/main/java/tn/esprit/tpfoyer/service/BlocServiceImpl.java
+++ src/main/java/tn/esprit/tpfoyer/service/BlocServiceImpl.java
@@ -1,16 +1,11 @@
 package tn.esprit.tpfoyer.service;
 
 
-import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import tn.esprit.tpfoyer.entity.Bloc;
 import tn.esprit.tpfoyer.repository.BlocRepository;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @AllArgsConstructor
--- src/main/java/tn/esprit/tpfoyer/service/UniversiteServiceImpl.java
+++ src/main/java/tn/esprit/tpfoyer/service/UniversiteServiceImpl.java
@@ -3,11 +3,7 @@ package tn.esprit.tpfoyer.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import tn.esprit.tpfoyer.entity.Universite;
 import tn.esprit.tpfoyer.repository.UniversiteRepository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
