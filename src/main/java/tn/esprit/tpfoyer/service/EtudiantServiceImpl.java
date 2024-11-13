a40bee9f1188647eb9ee3b3c58d89717245667ab
chirazsnoussi <80221258+chirazsnoussi@users.noreply.github.com>
2024-11-13 21:28:58 +0100

    Docker postman

--- src/main/java/tn/esprit/tpfoyer/service/EtudiantServiceImpl.java
+++ src/main/java/tn/esprit/tpfoyer/service/EtudiantServiceImpl.java
@@ -15,25 +15,6 @@ public class EtudiantServiceImpl implements IEtudiantService {
 
     EtudiantRepository etudiantRepository;
 
-    public List<Etudiant> retrieveAllEtudiants() {
-        return etudiantRepository.findAll();
-    }
-    public Etudiant retrieveEtudiant(Long etudiantId) {
-        return etudiantRepository.findById(etudiantId).get();
-    }
-    public Etudiant addEtudiant(Etudiant c) {
-        return etudiantRepository.save(c);
-    }
-    public Etudiant modifyEtudiant(Etudiant c) {
-        return etudiantRepository.save(c);
-    }
-    public void removeEtudiant(Long etudiantId) {
-        etudiantRepository.deleteById(etudiantId);
-    }
-    public Etudiant recupererEtudiantParCin(long cin)
-    {
-        return etudiantRepository.findEtudiantByCinEtudiant(cin);
-    }
 
 
 
